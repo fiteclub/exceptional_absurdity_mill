2123bdf76ce35b019e98c02c67154c1839d6ff70
fiteclub <mattf255@yahoo.com>
2021-07-02 14:49:31 -0400

    Satisfy new hash_array specs

--- lib/exceptional_absurdity_mill.rb
+++ lib/exceptional_absurdity_mill.rb
@@ -9,19 +9,19 @@ module ExceptionalAbsurdityMill
 
   class Base
     def self.hash_array(number_of_columns, number_of_rows)
-      keys = number_of_columns.times.each_with_object([]) do |n, keys_array|
-        keys_array[n] = "lorem"
+      column_names = number_of_columns.times.each_with_object([]) do |n, keys_array|
+        keys_array[n] = "lorem #{n}" 
       end
 
       number_of_rows.times.each_with_object([]) do |n, new_array|
-        new_array[n] = number_of_rows.times.each_with_object({}) do |i, new_hash |
-          new_hash[keys[i]] = "ipsum"
+        new_array[n] = column_names.each_with_object({}) do |column, new_hash |
+          new_hash[column] = "ipsum"
         end
       end
     end
 
     def self.random
-      random_string = "This is your random string"
+      "This is your random string"
     end
   end
 end
